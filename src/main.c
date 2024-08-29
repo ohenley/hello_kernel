d00d886dc4b445521e6b4d8af61b74077ff67cb6
olivier henley <olivier.henley@gmail.com>
2024-08-29 15:51:27 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,27 +2,23 @@
 #define _LINUX_CORE_
 
 #include <linux/module.h>
-#include <linux/kernel.h> // Needed for printk()
-#include <linux/init.h>   // Needed for the macros
+#include <linux/kernel.h>
+#include <linux/init.h>
 
-// Forward declarations of Ada functions
 extern void ada_init_module(void);
 extern void ada_cleanup_module(void);
 
-// Module initialization function
 static int __init my_init(void)
 {
     ada_init_module();
     return 0;
 }
 
-// Module cleanup function
 static void __exit my_exit(void)
 {
     ada_cleanup_module();
 }
 
-// Specify the init and exit functions
 module_init(my_init);
 module_exit(my_exit);
 
